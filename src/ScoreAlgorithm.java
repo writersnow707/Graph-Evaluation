a9c589a66bb8e93610ae280f9982f20da1f58e40
snow707 <90182653+writersnow707@users.noreply.github.com>
2022-08-31 16:53:07 +0900

    Update ScoreAlgorithm.java
    package changed by 2022-08-31

--- src/ScoreAlgorithm.java
+++ src/ScoreAlgorithm.java
@@ -1,9 +1,9 @@
-package writersnow707.scoreAlgorithm;
-
 import java.util.Random;
 import java.util.Scanner;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -35,7 +35,7 @@ public class ScoreAlgorithm {
 		
 		for (int i = 1; i <= data/10; i++) {
 			n = Math.abs(random.nextInt() % max);
-			score = Math.abs((random.nextInt() % 11) / 5.0);
+			score = Math.abs((random.nextInt() % 50) / 10.0) + 5.0;
 			bw.write(i + ", " + arr[n] + ", " + score);
 			bw.newLine();
 			
@@ -46,18 +46,76 @@ public class ScoreAlgorithm {
 		bw.flush();
 		bw.close();
 	}
+
+	private void changeGraphFile(String type, int data, int pair) throws IOException {
+		String filePath1 = null;
+		String filePath2 = null;
+		String s = null;
+		int v1 = -1;
+		int v2 = -1;
+		double score = -1;
+		int index = -1;
+		// File file1 = new File(filePath1);
+
+		Random rand = new Random();
+
+		for (int i = 2; i <= pair; i++) {
+			for (int j = 1; j <= 10; j++) {
+				index = 1;
+				filePath1 = "F://InfoLAB Seminar//" + type + "//" + type + "_" + data + "_" + i + " (" + j + ").txt";
+				filePath2 = "F://InfoLAB Seminar//WeightedGraph//" + type + "//" + type + "_" + data + "_" + i + " ("
+						+ j + ").txt";
+
+				File file = new File(filePath2);
+
+				if (!file.exists()) {
+					file.createNewFile();
+				}
+				BufferedReader br = new BufferedReader(new FileReader(filePath1));
+				BufferedWriter bw = new BufferedWriter(new FileWriter(file, true));
+				bw.write("id, data1, data2");
+				bw.newLine();
+
+				br.readLine();
+				try {
+					while (true) {
+						s = br.readLine();
+
+						v1 = Integer.parseInt(s.split(", ")[1]);
+						v2 = Integer.parseInt(s.split(", ")[2]);
+						score = Math.abs((rand.nextInt() % 40) / 10.0) + 1.0;
+
+						bw.write(index + ", " + v1 + ", " + v2 + ", " + score);
+						bw.newLine();
+
+						index++;
+					}
+				} catch (NullPointerException e) {
+					System.out.println("CREATE SCORE FILE");
+				} finally {
+					System.out.println("File Directory -> " + filePath2);
+					br.close();
+					bw.close();
+				}
+			}
+		}
+	}
 	
 	private void mainProject() throws IOException {
 		String type = null;
 		int data = -1;
+		int pair = -1;
 		System.out.println("CREATE SCORE FILE...");
 		
 		while (true) {
+			System.out.print("type / data / pair >> ");
 			type = sc.next();
 			data = sc.nextInt();
+			pair = sc.nextInt();
 		
 			String filePath = "F://InfoLAB Seminar//scoring//" + type + "_" + data + ".txt";
-			createScoreFile(filePath, data);
+			// createScoreFile(filePath, data);
+			changeGraphFile(type, data, pair);
 		}
 	}
 	
